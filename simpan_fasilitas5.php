e5b10f7cb88f9b94160dcff8409ac6d2bc5e3412
ferdhybagus <ferdhybagus93@gmail.com>
2025-08-13 11:30:39 +0700

    ss

--- simpan_fasilitas5.php
+++ simpan_fasilitas5.php
@@ -4,7 +4,7 @@ $query = "SELECT * from fasilitas5 ";
 if (isset($_POST['fasilitas5'])) {
     $fasilitas5 = mysqli_real_escape_string($conn, $_POST['fasilitas5']);
     
-    $query = "UPDATE fasilitas5 SET fasilitas5='$fasilitas5'";
+    $query = "UPDATE fasilitas5 SET fasilitas5s='$fasilitas5'";
     if (mysqli_query($conn, $query)) {
         echo "Fasilitas berhasil diperbarui!";
     }
